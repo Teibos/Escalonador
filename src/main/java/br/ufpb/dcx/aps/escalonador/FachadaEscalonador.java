4ddfb1389924e89e9d30650f498b6978f297df9a
Thallyson <thallysonliveira@gmail.com>
2019-07-17 21:50:50 -0300

    Funcionando o 1, 2 e 3

--- src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
@@ -1,24 +1,77 @@
 package br.ufpb.dcx.aps.escalonador;
 
+import java.util.ArrayList;
+
 public class FachadaEscalonador {
+	
+	private int quantum;
+	private int tick;
+	private TipoEscalonador tipoEscalonador;
+	private ArrayList<String> listaProcesso;
+	private String rodando;
+	private ArrayList<String> processoBloqueado;
+	
 
 	public FachadaEscalonador(TipoEscalonador tipoEscalonador) {
+		this.quantum = 3;
+		this.tick = 0;
+		this.rodando = "";
+		this.tipoEscalonador = tipoEscalonador;
+		this.listaProcesso = new ArrayList<String>();
+		this.processoBloqueado = new ArrayList<String>();
+		
+				
+		
 	}
 
 	public FachadaEscalonador(TipoEscalonador roundrobin, int quantum) {
 	}
 
 	public String getStatus() {
-		return null;
+		
+		if(this.listaProcesso.size() == 0) {
+			return ("Escalonador " + this.tipoEscalonador + ";"
+					+ "Processos: {"+this.rodando+"};"
+					+ "Quantum: " + this.quantum + ";"
+					+ "Tick: " + this.tick);
+		}
+		else {
+			return ("Escalonador " + this.tipoEscalonador + ";"
+					+ "Processos: {Fila: "+ this.listaProcesso.toString()+"};"
+					+ "Quantum: " + this.quantum + ";"
+					+ "Tick: " + this.tick);
+		}
+		
+		//return null;
+		
 	}
 
 	public void tick() {
+		this.tick ++;
+		if(this.listaProcesso.size() != 0) {
+			this.rodando = "Rodando: "+this.listaProcesso.get(0);
+			this.listaProcesso.remove(0);
+		}
+		
 	}
 
 	public void adicionarProcesso(String nomeProcesso) {
+		this.listaProcesso.add(nomeProcesso);
+		
 	}
 
 	public void finalizarProcesso(String nomeProcesso) {
+		boolean gato = true;
+		
+		for(String x: this.listaProcesso) {
+			if(x == nomeProcesso) {
+				gato = false;
+				this.listaProcesso.remove(x);
+			}
+		}if(gato) {
+			this.rodando = "Rodando: ";
+		}
+		
 	}
 
 	public void bloquearProcesso(String nomeProcesso) {
